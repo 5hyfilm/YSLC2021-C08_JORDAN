b232136749925d05e78fed9ae1acd4cda9189462
Waranthorn Chansawang <waranthorn_c@outlook.com>
2021-08-28 17:17:55 +0700

    add Application Insights JavaScript SDK
    https://docs.microsoft.com/en-us/azure/azure-monitor/app/usage-overview

--- monitor.py
+++
@@ -1,7 +0,0 @@
-import logging
-from opencensus.ext.azure.log_exporter import AzureEventHandler
-
-logger = logging.getLogger(__name__)
-logger.addHandler(AzureEventHandler(connection_string='InstrumentationKey=dbbde929-464a-41c7-b963-d299472157c4'))
-logger.setLevel(logging.INFO)
-logger.info('Hello, World!')
